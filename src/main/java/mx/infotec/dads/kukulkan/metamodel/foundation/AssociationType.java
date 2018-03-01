c3326d2ca4d39abd682f2a6cba0854e7e4a498ff
Daniel Cortes Pichardo <daniel.cortes.pichardo@gmail.com>
2018-02-28 23:48:27 -0600

    adding support for associations
    adding support for associations

---
+++ src/main/java/mx/infotec/dads/kukulkan/metamodel/foundation/AssociationType.java
@@ -0,0 +1,18 @@
+package mx.infotec.dads.kukulkan.metamodel.foundation;
+
+/**
+ * AssociationType.
+ *
+ * @author Daniel Cortes Pichardo
+ */
+public enum AssociationType {
+
+    /** The One to one. */
+    ONE_TO_ONE,
+    /** The One to many. */
+    ONE_TO_MANY,
+    /** The Many to one. */
+    MANY_TO_ONE,
+    /** The Many to many. */
+    MANY_TO_MANY
+}
---
+++ src/main/java/mx/infotec/dads/kukulkan/metamodel/foundation/EntityAssociation.java
@@ -0,0 +1,44 @@
+package mx.infotec.dads.kukulkan.metamodel.foundation;
+
+import java.io.Serializable;
+
+/**
+ * Entity Association.
+ *
+ * @author Daniel Cortes Pichardo
+ */
+public class EntityAssociation implements Serializable {
+
+    /** The Constant serialVersionUID. */
+    private static final long serialVersionUID = 1L;
+
+    private boolean ownerSide;
+
+    private Entity to;
+
+    private AssociationType type;
+
+    public boolean isOwnerSide() {
+        return ownerSide;
+    }
+
+    public Entity getTo() {
+        return to;
+    }
+
+    public AssociationType getType() {
+        return type;
+    }
+
+    public void setOwnerSide(boolean ownerSide) {
+        this.ownerSide = ownerSide;
+    }
+
+    public void setTo(Entity to) {
+        this.to = to;
+    }
+
+    public void setType(AssociationType type) {
+        this.type = type;
+    }
+}
