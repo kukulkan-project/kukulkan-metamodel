51c1b537600a5120226b43cd78da296cc36cd4a0
Roberto Villarejo Martinez <robertovillarejo@outlook.com>
2018-10-02 19:14:53 -0500

    Update PhysicalReferenceNameStrategy interface

--- src/main/java/mx/infotec/dads/kukulkan/metamodel/conventions/PhysicalReferenceNameStrategy.java
+++ src/main/java/mx/infotec/dads/kukulkan/metamodel/conventions/PhysicalReferenceNameStrategy.java
@@ -1,5 +1,7 @@
 package mx.infotec.dads.kukulkan.metamodel.conventions;
 
+import mx.infotec.dads.kukulkan.metamodel.foundation.EntityAssociation;
+
 /**
  * Primary Key Name Strategy
  * 
@@ -9,11 +11,12 @@ package mx.infotec.dads.kukulkan.metamodel.conventions;
 @FunctionalInterface
 public interface PhysicalReferenceNameStrategy {
 
-    /**
-     * Get the name of the primary key
-     * 
-     * @param entity
-     * @return
-     */
-    String getPhysicalReferenceName(String snakeConventionName);
+	/**
+	 * Get the name of the primary key
+	 * 
+	 * @param entity
+	 * @return
+	 */
+
+	String getPhysicalReferenceName(EntityAssociation entityAssociation, boolean toSource);
 }
